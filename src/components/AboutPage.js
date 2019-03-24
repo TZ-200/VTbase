ca2aa139b90cb6866d4e7bb5939c200949003168
Kyohei-I <ikyohei2100@gmail.com>
2019-03-24 18:50:56 +0900

    About page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 const AboutPage = () => (
   <div style={{
-    textAlign: 'center',
-    margin: 'auto',
-    fontSize: '6rem'
+    margin: '5rem'
   }}>
-    About（仮）
+
+    <h2>About</h2>
+    <h4>ポートフォリオとして作成したオリジナルのミニプロジェクトになります。2019年3月4日時点で取得した、800名弱のVtuber情報を表示するデータベースサイトです。バックエンドやパフォーマンスの向上まで手掛けたかったのですが、諸々の事情で手が出せませんでした。フロントエンドメインのポートフォリオとしてご覧ください。また、扱っている情報自体は当時のリアルのものですが、中途半端にランダムなデータが表示されていますので、基本的にデータの信憑性はないと考えてください。</h4>
+    <br/>
+
+    <h2>使用技術</h2>
+    <h4>HTML / CSS / Sass / JavaScript / React / Redux / Router / Nodejs / Firebase / Webpack</h4>
+    <br/>
+
+    <h5>※ 機械学習でVtuberをクラスタリングして2次元マッピングしたけど特に面白くなさそうなので断念したプロトタイプはこちら ⇒ <a href="https://vtbase.herokuapp.com">Demo</a></h5>
+
   </div>
 );
 
